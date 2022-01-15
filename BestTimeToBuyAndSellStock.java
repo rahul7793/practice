cac5b446863bdd6824f612a4fcacddb44482d826
Rahul Kumar Sharma <rahulsharma.7793@gmail.com>
2022-01-15 17:14:13 -0600

    Create BestTimeToBuyAndSellStock.java

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,47 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+  
+  
+  class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minBuy = Integer.MAX_VALUE;
+
+        for(int i = 0; i< prices.length; i++){
+            if(prices[i] < minBuy){
+                minBuy = prices[i];
+            }else if(maxProfit < (prices[i] - minBuy)){
+                maxProfit = prices[i] - minBuy;
+            }
+        }
+      //correct solution but time limit exceeds
+      //         List<Integer> list = Arrays.stream(prices).boxed().collect(Collectors.toList());
+//         while((list.size() - 1) != 0){
+//             int cur = list.get(0);
+//             list.remove(0);
+            
+//             int maxSale = Collections.max(list);
+//             maxProfit = Math.max(maxProfit, maxSale-cur);
+//         }
+        
+      
+        return maxProfit;
+    }
+}
