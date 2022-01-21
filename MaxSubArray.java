fe3cfe9b4ba280a3fc522c64640ded39ba3d67de
Rahul Kumar Sharma <rahulsharma.7793@gmail.com>
2022-01-21 03:44:56 -0600

    Add files via upload

---
+++ MaxSubArray.java
@@ -0,0 +1,61 @@
+public class MaxSubArray {
+
+//	private static int[] numsArray;
+
+	public static void main(String[] args) {
+		System.out.println(maxSubArray(new int[] { -2, -1 }));
+	}
+
+	public static int maxSubArray(int[] nums) {
+		// Kadane's Algo
+		int current_sub = nums[0];
+		int max_sum = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			current_sub = Math.max(nums[i], current_sub + nums[i]);
+			max_sum = Math.max(max_sum, current_sub);
+		}
+		return max_sum;
+
+//		DP - divide & conquer
+//numsArray = nums;
+//		return maxSub(0, nums.length-1);
+//
+//	}
+//
+//	public static int maxSub(int left, int right) {
+//		if (left > right)
+//			return Integer.MIN_VALUE;
+//
+//		int mid = Math.floorDiv(left + right, 2);
+//        int curr = 0;
+//        int bestLeftSum = 0;
+//        int bestRightSum = 0;
+//        
+//        // Iterate from the middle to the beginning.
+//        for (int i = mid - 1; i >= left; i--) {
+//            curr += numsArray[i];
+//            bestLeftSum = Math.max(bestLeftSum, curr);
+//        }
+//        
+//        // Reset curr and iterate from the middle to the end.
+//        curr = 0;
+//        for (int i = mid + 1; i <= right; i++) {
+//            curr += numsArray[i];
+//            bestRightSum = Math.max(bestRightSum, curr);
+//        }
+//        
+//        // The bestCombinedSum uses the middle element and the best
+//        // possible sum from each half.
+//        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
+//        
+//        // Find the best subarray possible from both halves.
+//        int leftHalf = maxSub(left, mid - 1);
+//        int rightHalf = maxSub(mid + 1, right);
+//        
+//        // The largest of the 3 is the answer for any given input array.
+//        return Math.max(bestCombinedSum, Math.max(leftHalf, rightHalf));
+
+	}
+
+}
